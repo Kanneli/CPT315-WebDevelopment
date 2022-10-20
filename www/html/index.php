816398cb61e3c66f3139f7a0525163d3695fef13
joodh.naseer <joodh.naseer@mma.gov.mv>
2022-10-20 22:04:15 +0500

    minor work

--- www/html/index.php
+++ www/html/index.php
@@ -4,6 +4,14 @@
 </head>  
 
 <body>  
-    <h1>Hello World! Changes are Applied</h1>
+    <h1>Hello World!</h1>
     <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
+
+    <?php
+    $ages['Peter'] = 32;
+    $ages['Quack'] = 34;
+    $ages['Joe'] = 30;
+
+    echo "Peter is " . $ages['Peter'] . " years old"
+    ?>
 </body>
